f1a2d844e13d4f8c9a0f24b8dbad7acf5e6a406c
Stanislav Leonchik <leonstas1@mail.ru>
2023-10-24 10:33:24 +0300

    Add Auth Controllers

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class RegistrationController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/welcome', to: 'home#welcome'
+
+  get '/registration', to: 'registration#new'
+  post '/registration', to: 'registration#create'
+
+  get '/login', to: 'login#new'
+  post '/login', to: 'login#create'
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

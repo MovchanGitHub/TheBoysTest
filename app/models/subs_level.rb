da27577349f85426541ba9f3f2a1614ae59cc037
Stanislav Leonchik <leonstas1@mail.ru>
2023-10-31 10:06:35 +0300

    Update Models

---
+++ app/models/subs_level.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Subs_Level < ApplicationRecord
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_secure_password
 
+  has_one :subs_level
 end

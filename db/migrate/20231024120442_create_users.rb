eb76b618562207a2b4be7aba32ad9967a7efbb59
Stanislav Leonchik <leonstas1@mail.ru>
2023-10-31 10:06:00 +0300

    Update Migration

--- db/migrate/20231024120442_create_users.rb
+++ db/migrate/20231024120442_create_users.rb
@@ -2,8 +2,14 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
 
-      t.string :email, uniqueness: true
-      t.string :name, null: false
+      t.string :email, uniqueness: true, null: false
+      t.string :password_digest
+      t.integer :rating
+      t.integer :level
+      t.references :subs_level_id
+      t.string :photo_link
+      t.integer :tests_passed_today
+
       t.timestamps
     end
   end

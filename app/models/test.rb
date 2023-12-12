737901d5f45c96ec97897ec21d40227f893d1c5a
Dexight <tupikin@sfedu.ru>
2023-12-12 22:05:12 +0300

    skeleton creating test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,23 @@
 class Test < ApplicationRecord
     belongs_to :genre
+    has_many :questions, dependent: :destroy
+
+    accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
+
+    validates :name, presence: true
+    validates :code_word, presence: true
+    validates :description, presence: true
+    validates :genre, presence: true
+
+    # Метод для создания теста с вопросами и ответами из переданных параметров
+    def self.create_with_questions(params)
+      test = create(params.except(:questions_attributes))
+      test.update(updated_at: Time.now, created_at: Time.now)
+
+      params[:questions_attributes].each do |question_params|
+        test.questions.create(question_params)
+      end
+
+      test
+    end
 end

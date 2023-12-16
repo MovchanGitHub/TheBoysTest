282bf45108e13f6b50a8d52dbc2eaf62e359697f
Dexight <tupikin@sfedu.ru>
2023-12-16 18:18:47 +0300

    rewrite form.html.erb & update tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,4 +6,19 @@ class TestsController < ApplicationController
     end
     @tests = @tests.where(genre_id: params[:genre]) if params[:genre].present?
   end
+
+  def create
+    test_params = Hash.new
+    test_params[:name] = params.require(:name)
+    test_params[:code_word] = params.require(:code_word)
+    test_params[:description] = params.require(:description)
+    test_params[:genre_id] = params.require(:genre_id)
+    #test_params[:questions_attributes] = Hash.new
+
+    Test.create(test_params)
+    puts "putting"
+    flash[:notice] = 'Test was successfully created.'
+    redirect_to '/tests'
+
+  end
 end

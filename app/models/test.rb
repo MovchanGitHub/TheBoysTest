aa77a12e376bc472c7fd2c423f616b558e18a357
Dexight <tupikin@sfedu.ru>
2023-12-16 18:28:04 +0300

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,16 +8,4 @@ class Test < ApplicationRecord
     validates :code_word, presence: true
     validates :description, presence: true
     validates :genre, presence: true
-
-    # Метод для создания теста с вопросами и ответами из переданных параметров
-    def self.create_with_questions(params)
-      test = create(params.except(:questions_attributes))
-      test.update(updated_at: Time.now, created_at: Time.now)
-
-      params[:questions_attributes].each do |question_params|
-        test.questions.create(question_params)
-      end
-
-      test
-    end
 end

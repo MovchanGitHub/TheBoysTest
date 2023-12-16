7139df956bcbb926931ed5c93befd7d1fa7eefc3
Dexight <tupikin@sfedu.ru>
2023-12-17 01:19:51 +0300

    edit genre choosing

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -7,18 +7,24 @@ class TestsController < ApplicationController
     @tests = @tests.where(genre_id: params[:genre]) if params[:genre].present?
   end
 
+  def new
+    @genres = Genre.all
+    @test = Test.new
+  end
+
   def create
-    test_params = Hash.new
-    test_params[:name] = params.require(:name)
-    test_params[:code_word] = params.require(:code_word)
-    test_params[:description] = params.require(:description)
-    test_params[:genre_id] = params.require(:genre_id)
-    #test_params[:questions_attributes] = Hash.new
+    #test_params = Hash.new
+    #test_params[:name] = params.require(:name)
+    #test_params[:code_word] = params.require(:code_word)
+    #test_params[:description] = params.require(:description)
+    #test_params[:genre_id] = params.require(:genre_id)
 
     Test.create(test_params)
-    puts "putting"
     flash[:notice] = 'Test was successfully created.'
     redirect_to '/tests'
-
   end
+end
+
+def test_params
+  params.require(:test).permit(:name, :code_word, :description, :genre_id)
 end

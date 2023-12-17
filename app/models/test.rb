52cd08d60db1d4d4c9b5fbf76cdb626cf5204b96
MovchanGitHub <92666028+MovchanGitHub@users.noreply.github.com>
2023-12-17 18:15:54 +0300

    Dev (#7)
    * skeleton creating test
    
    * add styles for test creating
    
    * Add dynamic question spawn in create form
    
    * add some styles
    
    * Question removing
    
    * add dynamic answer spawn in create form
    
    * add non-deleted 1 question + 2 answer
    
    * add margins
    
    * Update index.html.erb
    
    delete log in button
    
    * rewrite form.html.erb & update tests_controller
    
    * Update test.rb
    
    * edit genre choosing
    
    * add code_word=nil & * to important fields
    
    * Fix Tests Text Align
    
    * Delete Option to Create Tests for Unsigned Users
    
    ---------
    
    Co-authored-by: Dexight <tupikin@sfedu.ru>

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,8 @@
 class Test < ApplicationRecord
     belongs_to :genre
+
+    validates :name, presence: true
+    validates :code_word, presence: false
+    validates :description, presence: true
+    validates :genre, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :tests
   devise_for :users
   root 'home#index'
   get '/welcome', to: 'home#welcome'
@@ -49,4 +50,5 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
+
 end

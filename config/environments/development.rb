cca0659d295f4a9a920bcf77e7b9fbb0c4a29458
Stanislav Leonchik <leonstas1@mail.ru>
2023-12-23 18:26:11 +0300

    Add Mailer Host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://the-boys-test.onrender.com', port: 3000 }
 end
